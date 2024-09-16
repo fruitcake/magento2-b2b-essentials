90a18757180df81b13d2a4311e2ee8047e3fafdb
Barry vd. Heuvel <barry@fruitcake.nl>
2024-09-16 12:16:49 +0200

    Renamve observer

--- Observer/QuoteProductObserver.php
+++ Observer/QuoteProductObserver.php
@@ -7,7 +7,7 @@ use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
 use Magento\Framework\Exception\LocalizedException;
 
-class DisableAddToCart implements ObserverInterface
+class QuoteProductObserver implements ObserverInterface
 {
 
     /** @var $helper */
